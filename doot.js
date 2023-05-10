0a80aeb1f84e7185ab7591fb0aadca09aa7b4423
letters <46039037+keli5@users.noreply.github.com>
2023-05-09 19:27:50 -0500

    doot

--- doot.js
+++ doot.js
@@ -1,6 +1,7 @@
 var pattern = ['d', 'o', 'o', 't'];
 var current = 0;
-var dooted = false;
+var dooted = 0;
+var elements = document.getElementsByTagName("*") 
 
 var keyHandler = function (event) {
 	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
@@ -11,12 +12,19 @@ var keyHandler = function (event) {
 
 	if (pattern.length === current) {
 		current = 0;
-		dooted = !dooted;
-        if (dooted) {
+		dooted = (dooted <= 2) ? (dooted + 1) : 0
+        if (dooted == 1) {
             document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/293340619395563521/1105641603219079189/dootdoot.gif')"
+        } else if (dooted == 2) {
+            for (element of elements) {
+				element.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/293340619395563521/1105641603219079189/dootdoot.gif')"
+			}
         } else {
-            document.body.style.backgroundImage = "none"
-        }
+			document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/293340619395563521/1105641603219079189/dootdoot.gif')"
+			for (element of elements) {
+				element.style.backgroundImage = "none"
+			  } 
+		}
 	}
 
 };
